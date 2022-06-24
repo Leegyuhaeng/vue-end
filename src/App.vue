ebfb907d48ee57eec7b4e8555c4dc84b9e4193e0
Leegyuhaeng <rbgodsla21@fixelsoft.com>
2022-06-24 19:54:41 +0900

    push

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
 	<div>
-		<header>
-			<router-link to="/login">로그인</router-link> | 
-			<router-link to="/signup">회원가입</router-link>
-		</header>
+		<app-header></app-header>
 		<router-view>
 			
 		</router-view>
@@ -11,9 +8,13 @@
 </template>
 
 <script>
-	export default {
-		
+import AppHeader from '@/components/common/AppHeader.vue'
+
+export default {
+	components : {
+		AppHeader
 	}
+}
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+        <label for="username">id: </label>
+        <input id="username" type="text" v-model="username">
+    </div>
+    
+    <div>
+        <label for="password">pw: </label>
+        <input type="password" v-model="password">
+    </div>
+    
+    <div>
+        <label for="nickname">nickname: </label>
+        <input type="text" v-model="nickname">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username:null,
+            password:null,
+            nickname:null,
+        }
+    },
+    methods: {
+        submitForm(){
+            alert("submit")
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
